e18914e94406dafdc12ed81c7f4c67eb4f0e3068
SokmontreyTDSB <100730097+SokmontreyTDSB@users.noreply.github.com>
2022-06-08 13:56:52 -0400

    Create posts.js

---
+++ posts/posts.js
@@ -0,0 +1,15 @@
+//post's detail order: ID, type, title, description, date
+// Date formate: mm/dd/yyyy
+const posts = `
+$$$ 10052022
+$ Soccerball
+$ Title 1
+$ Description 1
+$ 06/05/2022
+
+$$$ 15052022
+$ Volleyball
+$ Title 2
+$ Description 2
+$ 06/06/2022 
+`; 
