6f4aa6b10c4ec426e9706308e1ef7e63aa077864
SokmontreyTDSB <100730097+SokmontreyTDSB@users.noreply.github.com>
2022-06-12 22:02:43 -0400

    use MinifJS in the project

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-class App{}
+class Home extends Component{
+    constructor(){super()}
+    loadState(){
+        const button = `<button event='updateA' on='click'>ButtonClick</button>`
+        this.setVar({
+            test: button,
+            a: 1
+        })
+        this.setEvent({
+            updateA: ()=>{
+                this.setVar({
+                    a: this.var.a + 1
+                })
+            }
+        });
+    }
+}
+class App{
+    constructor(){
+        const pageManager = new PageManager();
+        pageManager.setPage({
+            home: new Home()
+        })
+        pageManager.render();
+    }
+}
+
+const app = new App();
