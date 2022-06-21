ff91d16028bc56233fb3edd8ec6e59aa769c42ab
SokmontreyTDSB <100730097+SokmontreyTDSB@users.noreply.github.com>
2022-06-21 12:40:49 -0400

    Add files via upload

--- posts/posts.js
+++ posts/posts.js
@@ -4,28 +4,28 @@ const posts = `
 $$$ 06052022
 $ soccerball
 $ 6th of May Soccerball match
-$ On May 06 2022, Emery soccerball teams has a match with the team from ... with the final score of ...
+$ On May 6th 2022, Emery Soccerball teams has a match with the team from ... with the final score of ...
 $ 05/06/2022
 $ 7
 
 $$$ 13022020
 $ wrestling
-$ Wrestling match on 13 Feburary 2020
+$ Wrestling match on Feburary 13th 2020
 $ On Febuary 13th 2020, Emery wrestling teams has a match with the team from ...
 $ 02/13/2020
 $ 6
 
 $$$ 03122019
 $ volleyball
-$ Volleyball match on 03 December 2019
-$ On December 3th 2019, Emery Volleybal team has a match with the team from ...
+$ Volleyball match on December 3th 2019
+$ On December 3rd 2019, Emery Volleybal team has a match with the team from ...
 $ 12/03/2019
 $ 6
 
 $$$ 02122019
 $ basketball
 $ Basketball match: Emery vs ...
-$ On December 02 2019, Emery Collegiate Basketball teams has a match with the team from ... with the final score of ...
+$ On December 2nd 2019, Emery Collegiate Basketball teams has a match with the team from ... with the final score of ...
 $ 12/02/2019
 $ 10
 `;
